26df2d4a928b96b29b2c6c3b66fc53b035d216f1
Adrian Cirstei <adicirstei@gmail.com>
2015-12-24 21:53:33 +0200

    ch1

--- src/Main.purs
+++ src/Main.purs
@@ -4,6 +4,18 @@ import Prelude
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
 
+import Math (sqrt, pi)
+
+diagonal :: Number -> Number -> Number 
+diagonal w h =
+  sqrt (w*w + h*h)
+
+
+circleArea :: Number -> Number
+circleArea r =
+  pi * r * r
+
+
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+main = print (diagonal 3.0 4.0)
